5a8a9a417f67549bc562af8f2ef2221709faed1a
TankleL <Tankle_@hotmail.com>
2016-08-26 01:01:57 +0800

    project dost

---
+++ dost/dost/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+using GitSharp;
+
+namespace dost
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Repository m_repo;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnSelectRepo(object sender, RoutedEventArgs e)
+        {
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.Description = "选择仓库（文件夹）所在位置";
+            dlg.ShowNewFolderButton = false;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                m_repo = new Repository(dlg.SelectedPath);
+                m_historyGraph.Update(m_repo);
+            }
+        }
+
+        private void OnExit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
